ecc255d67871c71e0dab08204ad6ae9da7f8bda6
hnakn <23z223@psgtech.ac.in>
2026-06-24 20:34:55 +0530

    Add multi word search

--- backend/Services/SearchIndex.cs
+++ backend/Services/SearchIndex.cs
@@ -17,8 +17,16 @@ public class SearchIndex
 
     public HashSet<int> SearchDocuments(string word)
     {
-        if(!_index.ContainsKey(word)) return [];
-        return _index[word];
+        string[] words = word.Split();
+        if(!_index.ContainsKey(words[0])) return [];
+        HashSet<int> result = new(_index[words[0]]);
+        foreach(string s in words)
+        {
+            if(!_index.ContainsKey(s)) return [];
+            result.IntersectWith(_index[s]);
+        }
+        
+        return result;
     }
 
     public void RemoveDocument(Document document)
